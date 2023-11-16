4241bd90a045ef11842555aa4cbe586e0b39d5fb
amandadr <amandadroy@gmail.com>
2023-11-16 14:14:16 -0400

    create admin categories route

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,3 @@
-=begin
-Replace the text on the dashboard page with these general stats about the store:
-Display a count of how many products are in the database
-Display a count of how many categories are in the database
-=end
-
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
   def show
